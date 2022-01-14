75265d166d80e98452fac875a663432f930da96d
samjkennedy <samjameskennedy@gmail.com>
2022-01-14 08:43:24 +0000

    Support printing Ints

--- src/main/java/com/skennedy/rasna/compilation/LLVMCompiler.java
+++ src/main/java/com/skennedy/rasna/compilation/LLVMCompiler.java
@@ -1,10 +1,17 @@
 package com.skennedy.rasna.compilation;
 
-import com.skennedy.rasna.Rasna;
+import com.skennedy.rasna.typebinding.BoundExpression;
+import com.skennedy.rasna.typebinding.BoundFunctionArgumentExpression;
+import com.skennedy.rasna.typebinding.BoundFunctionDeclarationExpression;
+import com.skennedy.rasna.typebinding.BoundLiteralExpression;
+import com.skennedy.rasna.typebinding.BoundPrintExpression;
 import com.skennedy.rasna.typebinding.BoundProgram;
+import com.skennedy.rasna.typebinding.TypeSymbol;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.bytedeco.javacpp.BytePointer;
+import org.bytedeco.javacpp.Pointer;
+import org.bytedeco.javacpp.PointerPointer;
 import org.bytedeco.llvm.LLVM.LLVMBasicBlockRef;
 import org.bytedeco.llvm.LLVM.LLVMBuilderRef;
 import org.bytedeco.llvm.LLVM.LLVMContextRef;
@@ -14,10 +21,14 @@ import org.bytedeco.llvm.LLVM.LLVMValueRef;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.List;
+import java.util.stream.Collectors;
 
 import static org.bytedeco.llvm.global.LLVM.LLVMAbortProcessAction;
 import static org.bytedeco.llvm.global.LLVM.LLVMAddFunction;
 import static org.bytedeco.llvm.global.LLVM.LLVMAppendBasicBlockInContext;
+import static org.bytedeco.llvm.global.LLVM.LLVMBuildCall;
+import static org.bytedeco.llvm.global.LLVM.LLVMBuildGlobalStringPtr;
 import static org.bytedeco.llvm.global.LLVM.LLVMBuildRet;
 import static org.bytedeco.llvm.global.LLVM.LLVMCCallConv;
 import static org.bytedeco.llvm.global.LLVM.LLVMConstInt;
@@ -34,8 +45,10 @@ import static org.bytedeco.llvm.global.LLVM.LLVMInitializeNativeAsmParser;
 import static org.bytedeco.llvm.global.LLVM.LLVMInitializeNativeAsmPrinter;
 import static org.bytedeco.llvm.global.LLVM.LLVMInitializeNativeTarget;
 import static org.bytedeco.llvm.global.LLVM.LLVMInt32TypeInContext;
+import static org.bytedeco.llvm.global.LLVM.LLVMInt8TypeInContext;
 import static org.bytedeco.llvm.global.LLVM.LLVMLinkInMCJIT;
 import static org.bytedeco.llvm.global.LLVM.LLVMModuleCreateWithNameInContext;
+import static org.bytedeco.llvm.global.LLVM.LLVMPointerType;
 import static org.bytedeco.llvm.global.LLVM.LLVMPositionBuilderAtEnd;
 import static org.bytedeco.llvm.global.LLVM.LLVMPrintModuleToFile;
 import static org.bytedeco.llvm.global.LLVM.LLVMSetFunctionCallConv;
@@ -48,6 +61,10 @@ public class LLVMCompiler implements Compiler {
     private static final Logger log = LogManager.getLogger(LLVMCompiler.class);
 
     public static final BytePointer error = new BytePointer();
+
+    private LLVMTypeRef i32Type;
+    private LLVMValueRef printf;
+    private LLVMValueRef formatStr; //"%d\n"
 
     @Override
     public void compile(BoundProgram program, String outputFileName) throws IOException {
@@ -62,20 +79,36 @@ public class LLVMCompiler implements Compiler {
         LLVMContextRef context = LLVMContextCreate();
         LLVMModuleRef module = LLVMModuleCreateWithNameInContext(outputFileName, context);
         LLVMBuilderRef builder = LLVMCreateBuilderInContext(context);
-        LLVMTypeRef i32Type = LLVMInt32TypeInContext(context);
-        LLVMTypeRef mainType = LLVMFunctionType(i32Type, LLVMVoidType(), /* argumentCount */ 0, /* isVariadic */ 0);
 
-        LLVMValueRef main = LLVMAddFunction(module, "main", mainType);
-        LLVMSetFunctionCallConv(main, LLVMCCallConv);
+        i32Type = LLVMInt32TypeInContext(context);
 
-        LLVMBasicBlockRef entry = LLVMAppendBasicBlockInContext(context, main, "entry");
+        //Declare printf function and string formatter once
+        printf = LLVMAddFunction(module, "printf", LLVMFunctionType(i32Type, LLVMPointerType(LLVMInt8TypeInContext(context), 0), 1, 1));//No idea what AddressSpace is for yet
 
-        LLVMPositionBuilderAtEnd(builder, entry);
+        for (BoundExpression expression : program.getExpressions()) {
+            if (expression instanceof BoundFunctionDeclarationExpression) {
 
-        LLVMValueRef returnCode = LLVMConstInt(i32Type, 0, 0);
-        LLVMBuildRet(builder, returnCode);
+                BoundFunctionDeclarationExpression functionDeclarationExpression = (BoundFunctionDeclarationExpression) expression;
 
-        LLVMVerifyFunction(main, LLVMAbortProcessAction);
+                if (functionDeclarationExpression.getFunctionSymbol().getName().equals("main")) {
+                    //TODO: main args
+                    LLVMTypeRef mainType = LLVMFunctionType(i32Type, LLVMVoidType(), /* argumentCount */ 0, /* isVariadic */ 0);
+
+                    LLVMValueRef main = LLVMAddFunction(module, "main", mainType);
+                    LLVMSetFunctionCallConv(main, LLVMCCallConv);
+
+                    LLVMBasicBlockRef entry = LLVMAppendBasicBlockInContext(context, main, "entry");
+                    LLVMPositionBuilderAtEnd(builder, entry);
+
+                    visitMainMethod((BoundFunctionDeclarationExpression)expression, builder);
+
+                    LLVMValueRef returnCode = LLVMConstInt(i32Type, 0, 0);
+                    LLVMBuildRet(builder, returnCode);
+
+                    LLVMVerifyFunction(main, LLVMAbortProcessAction);
+                }
+            }
+        }
 
         //LLVMDumpModule(module);
         if (LLVMVerifyModule(module, LLVMAbortProcessAction, error) != 0) {
@@ -83,7 +116,6 @@ public class LLVMCompiler implements Compiler {
             return;
         }
 
-        //clang test.ll -o test.exe
         BytePointer llFile = new BytePointer("./" + outputFileName + ".ll");
         if (LLVMPrintModuleToFile(module, llFile, error) != 0) {
             log.error("Failed to write module to file");
@@ -111,47 +143,60 @@ public class LLVMCompiler implements Compiler {
             c = (char) errorStream.read();
         }
         if (stderr.length() != 0) {
-            log.error(stderr.toString());
+            log.warn(stderr.toString()); //gcc likes to put warnings in stderr, if there was an error we probably wouldn't have gotten this far
         }
         log.info("Compiled IR to " + outputFileName + ".exe");
-
-        // Stage 3: Dump the module to file
-//        if (LLVMWriteBitcodeToFile(module, "./" + outputFileName + ".bc") != 0) {
-//            log.error("Failed to write bitcode to file");
-//            return;
-//        }
-//
-//        // Stage 4: Create the relocatable object file
-//        BytePointer triple = LLVMGetDefaultTargetTriple();
-//        LLVMTargetRef target = new LLVMTargetRef();
-//
-//        if (LLVMGetTargetFromTriple(triple, target, error) != 0) {
-//            log.error("Failed to get target from triple: " + error.getString());
-//            LLVMDisposeMessage(error);
-//            return;
-//        }
-//
-//        String cpu = "generic";
-//        String cpuFeatures = "";
-//        int optimizationLevel = 0;
-//        LLVMTargetMachineRef tm = LLVMCreateTargetMachine(
-//                target, triple.getString(), cpu, cpuFeatures, optimizationLevel,
-//                LLVMRelocDefault, LLVMCodeModelDefault
-//        );
-//
-//        BytePointer outputFile = new BytePointer("./" + outputFileName + ".o");
-//        if (LLVMTargetMachineEmitToFile(tm, module, outputFile, LLVMObjectFile, error) != 0) {
-//            log.error("Failed to emit relocatable object file: " + error.getString());
-//            LLVMDisposeMessage(error);
-//            return;
-//        }
 
         // Stage 5: Dispose of allocated resources
         LLVMDisposeModule(module);
         LLVMDisposeBuilder(builder);
         LLVMContextDispose(context);
-//
-//        // Stage 6: Convert .o file to .exe
-//        Runtime.getRuntime().exec("gcc " + outputFileName + ".o -o " + outputFileName + ".exe");
+    }
+
+    private LLVMValueRef visit(BoundExpression expression, LLVMBuilderRef builder) {
+        switch (expression.getBoundExpressionType()) {
+            case LITERAL:
+                return visit((BoundLiteralExpression) expression, builder);
+            case PRINT_INTRINSIC:
+                return visit((BoundPrintExpression) expression, builder);
+            default:
+                throw new UnsupportedOperationException("LLVM Compilation for bound expression type " + expression.getBoundExpressionType() + " is not yet implemented in LLVM");
+        }
+    }
+
+    private LLVMValueRef visit(BoundLiteralExpression literalExpression, LLVMBuilderRef builder) {
+
+        if (literalExpression.getType() == TypeSymbol.INT) {
+            return LLVMConstInt(i32Type, (int) literalExpression.getValue(), 0);
+        }
+        throw new UnsupportedOperationException("Literals of type `" +literalExpression.getType() + "` are not yet supported in LLVM");
+    }
+
+    private LLVMValueRef visit(BoundPrintExpression printExpression, LLVMBuilderRef builder) {
+
+        if (formatStr == null) {
+            formatStr = LLVMBuildGlobalStringPtr(builder, "%d\n", "formatStr");
+        }
+
+        LLVMValueRef res = visit(printExpression.getExpression(), builder);
+        PointerPointer<Pointer> printArgs = new PointerPointer<>(2)
+                .put(0, formatStr)
+                .put(1, res);
+
+        return LLVMBuildCall(builder, this.printf, printArgs, 2, "");
+    }
+
+    private void visitMainMethod(BoundFunctionDeclarationExpression mainMethodDeclaration, LLVMBuilderRef builder) {
+        List<TypeSymbol> argumentTypes = mainMethodDeclaration.getArguments().stream()
+                .map(BoundFunctionArgumentExpression::getType)
+                .collect(Collectors.toList());
+
+        if (argumentTypes.size() == 1) {
+            throw new UnsupportedOperationException("Main method args are not yet implemented in LLVM");
+        }
+
+        for (BoundExpression expression : mainMethodDeclaration.getBody().getExpressions()) {
+            visit(expression, builder);
+        }
     }
 }
--- test.ll
+++ test.ll
@@ -1,7 +1,14 @@
 ; ModuleID = 'test'
 source_filename = "test"
 
+@formatStr = private unnamed_addr constant [4 x i8] c"%d\0A\00", align 1
+
+declare i32 @printf(i8*, ...)
+
 define i32 @main() {
 entry:
+  %0 = call i32 (i8*, ...) @printf(i8* getelementptr inbounds ([4 x i8], [4 x i8]* @formatStr, i32 0, i32 0), i32 1)
+  %1 = call i32 (i8*, ...) @printf(i8* getelementptr inbounds ([4 x i8], [4 x i8]* @formatStr, i32 0, i32 0), i32 2)
+  %2 = call i32 (i8*, ...) @printf(i8* getelementptr inbounds ([4 x i8], [4 x i8]* @formatStr, i32 0, i32 0), i32 2)
   ret i32 0
 }
