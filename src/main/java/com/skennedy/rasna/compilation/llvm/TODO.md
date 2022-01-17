6419e415738c14638715a1af379e442e9ca41e10
samjkennedy <samjameskennedy@gmail.com>
2022-01-17 21:07:44 +0000

    Add LLVM TODO.md

---
+++ src/main/java/com/skennedy/rasna/compilation/llvm/TODO.md
@@ -0,0 +1,7 @@
+## LLVM TODO
+
+- More types other than Int
+    - Other type BinOps
+- Arrays and tuples
+- Structs
+- Generics
--- src/main/java/com/skennedy/rasna/compilation/llvm/Visitor.java
+++
@@ -1,9 +0,0 @@
-package com.skennedy.rasna.compilation.llvm;
-
-import com.skennedy.rasna.typebinding.BoundExpression;
-import org.bytedeco.llvm.LLVM.LLVMValueRef;
-
-public abstract class Visitor<E extends BoundExpression> {
-
-    public abstract LLVMValueRef visit(E expression, Scope scope);
-}
